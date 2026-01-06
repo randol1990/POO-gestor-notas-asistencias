7b27a412ab78e99cff6cbdff294804f9bde522e5
Randol Vila Arévalo <116229707+randol1990@users.noreply.github.com>
2026-01-05 20:50:46 -0500

    Add files via upload

---
+++ core/estudiante.py
@@ -0,0 +1,65 @@
+import re
+
+class estudiante:
+    def __init__(self, id, nombre, a_paterno, a_materno, dni, fecha_nacimiento, sexo, email, celular):
+        self.__id = id
+        self.__nombre = nombre
+        self.__a_paterno = a_paterno
+        self.__a_materno = a_materno
+        self.__dni = dni
+        self.__fecha_nacimiento = fecha_nacimiento
+        self.__sexo = sexo
+        self.__cursos = []
+        self.__asistencias = []
+        self.__email = email
+        self.__celular = celular
+
+    #GETTERS Y SETTERS
+    @property
+    def id(self):
+        return self.__id
+    
+    @id.setter
+    def id(self, valor):
+        self.__id = valor
+    
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def id(self):
+        return self.__id
+    
